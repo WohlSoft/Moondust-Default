e3d9b5332bf215a49c0a444d3970e6ba6d653b23
Wohlstand <admin@wohlnet.ru>
2015-10-29 10:45:30 +0300

    Update of scripts - fixed some wrong celling slopes - written new npc-ai scripts

--- script/npcs/venus_firetrap.lua
+++ script/npcs/venus_firetrap.lua
@@ -1,20 +1,32 @@
-class 'piranha_plant'
+class 'venus_firetrap'
 
 local AI_SHOWING_UP = 0
 local AI_SHOWING_IDLE = 1
 local AI_HIDING_DOWN = 2
 local AI_HIDING_IDLE = 3
 
-function piranha_plant:hideSprite()
-        self.npc_obj:setSpriteWarp(1.0, 3, true)
+function venus_firetrap:hideSprite()
+        self.npc_obj:setSpriteWarp(1.0, NPC_WARP_BOTTOM, true)
 end
 
-function piranha_plant:updateWarp()
-        self.npc_obj:setSpriteWarp(1.0-(math.abs(self.npc_obj.top-self.npc_obj.bottom)/self.def_height), 3, true)
+function venus_firetrap:updateWarp()
+        self.npc_obj:setSpriteWarp(1.0-(math.abs(self.npc_obj.top-self.npc_obj.bottom)/self.def_height), NPC_WARP_BOTTOM, true)
+end
+
+function venus_firetrap:shot()
+    local bullet=self.npc_obj:spawnNPC(246, GENERATOR_APPEAR, SPAWN_UP, false)
+    bullet.speedX = self.npc_obj.direction * 2
+    bullet.center_x = self.npc_obj.center_x+self.npc_obj.direction*(self.npc_obj.width/2)
+    bullet.center_y = self.def_top+16
+    if((self.plrpos_detector:positionY()<self.def_centerY)) then
+        bullet.speedY = -1.5
+    elseif((self.plrpos_detector:positionY()>self.def_centerY)) then
+        bullet.speedY = 1.5
+    end
 end
 
 
-function piranha_plant:initProps()
+function venus_firetrap:initProps()
     -- Animation properties
 
     -- Top position (reset size to initial)
@@ -31,6 +43,7 @@ function piranha_plant:initProps()
 
     -- FOR AI_SHOWING_IDLE
     self.cur_showingIdleTicks = 0
+    self.shooted = false
 
     -- FOR AI_HIDING_DOWN
     self.cur_hidingDownTicks = 0
@@ -38,42 +51,67 @@ function piranha_plant:initProps()
     -- FOR AI_HIDING_IDLE
     self.cur_hidingIdleTicks = 0
 
+    self.curani = -1
+    self:refreshAnimation();
     self:updateWarp()
 end
 
-function piranha_plant:__init(npc_obj)
+function venus_firetrap:__init(npc_obj)
     self.npc_obj = npc_obj
+
     -- Config
     self.def_top = npc_obj.top
     self.def_height = npc_obj.height
     self.def_bottom = npc_obj.bottom
     self.speed = 1
+    self.def_centerY = self.npc_obj.center_y
+
+    self.curani = -1
+
+    self.def_frame_lookup = {1}
+    self.def_frame_lookdown = {0}
 
     -- Detector of player
     self.plr_detector = self.npc_obj:installInAreaDetector(-44, -600, 44, 600, {4})
+    self.plrpos_detector = self.npc_obj:installPlayerPosDetector()
 
     -- FOR AI_SHOWING_UP
     self.def_showingUpTicks = smbx_utils.ticksToTime(self.npc_obj.height)
 
     -- FOR AI_SHOWING_IDLE
-    self.def_showingIdleTicks = smbx_utils.ticksToTime(50)
+    self.def_showingIdleTicks = smbx_utils.ticksToTime(150)
+    self.def_shootDelayTicks = smbx_utils.ticksToTime(125)
 
     -- FOR AI_HIDING_DOWN
     self.def_hidingDownTicks = smbx_utils.ticksToTime(self.npc_obj.height)
 
     -- FOR AI_HIDING_IDLE
-    self.def_hidingIdleTicks = smbx_utils.ticksToTime(42)
+    self.def_hidingIdleTicks = smbx_utils.ticksToTime(90)
 
     npc_obj.gravity = 0
 
     self:initProps()
 end
 
-function piranha_plant:onActivated()
+function venus_firetrap:onActivated()
     self:initProps()
 end
 
-function piranha_plant:onLoop(tickTime)
+function venus_firetrap:refreshAnimation()
+    if((self.plrpos_detector:positionY()<self.def_centerY) and (self.curani~=0)) then
+        self.npc_obj:setSequence(self.def_frame_lookup)
+        self.curani=0
+    elseif((self.plrpos_detector:positionY()>self.def_centerY) and (self.curani~=1)) then
+        self.npc_obj:setSequence(self.def_frame_lookdown)
+        self.curani=1
+    end
+end
+
+function venus_firetrap:onLoop(tickTime)
+
+    self.npc_obj.direction = self.plrpos_detector:directedTo()
+    self:refreshAnimation()
+
     if(self.cur_mode == AI_SHOWING_UP)then
         if(self.def_showingUpTicks > self.cur_showingUpTicks)then
             self.cur_showingUpTicks = self.cur_showingUpTicks + tickTime
@@ -84,10 +122,15 @@ function piranha_plant:onLoop(tickTime)
             self.npc_obj.top = self.def_top
             self.npc_obj:resetSpriteWarp()
             self.cur_showingUpTicks = 0
+            self.shooted = false;
         end
     elseif(self.cur_mode == AI_SHOWING_IDLE)then
         if(self.def_showingIdleTicks >= self.cur_showingIdleTicks)then
             self.cur_showingIdleTicks = self.cur_showingIdleTicks + tickTime
+            if((not self.shooted) and (self.def_shootDelayTicks <= self.cur_showingIdleTicks))then
+                self:shot()
+                self.shooted = true
+            end
         else
             self.cur_mode = AI_HIDING_DOWN
             self.cur_showingIdleTicks = 0
@@ -118,4 +161,5 @@ function piranha_plant:onLoop(tickTime)
     end
 end
 
-return piranha_plant
+return venus_firetrap
+
